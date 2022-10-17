e4b66d3bcafe52ff90f48a81af90b32a118948a8
Ivan Silva <msilva18@ucol.mx>
2022-10-17 14:23:27 -0500

    os.path to pathlib.Path

--- isocket.py
+++ isocket.py
@@ -1,97 +1,117 @@
-
-import os
 import pyinotify
-import sys
+from pathlib import Path
+
 
 class Socket(pyinotify.ProcessEvent):
 
-	""" Socket simulation uses a directory scanner for new incoming data simulation on files """
+    """ Socket simulation uses a directory scanner for new incoming data simulation on files """
 
-	def __init__(self):
+    def __init__(self):
 
-		# The file used to scan and get new data
-		self.file = None
+        # The file used to scan and get new data
+        self.file = None
 
-		# A queue for new incoming data
-		self.msg_queue = []
+        # A queue for new incoming data
+        self.msg_queue = []
 
-		# The socket begins the scanning for its file for new incoming data.
-		self.listening = False
+        # The socket begins the scanning for its file for new incoming data.
+        self.listening = False
 
-		# Used when text written into the file its new.
-		# Allows the data flow blocking if not changed.
-		self.is_ignored = True
+        # Used when text written into the file its new.
+        # Allows the data flow blocking if not changed.
+        self.is_ignored = True
 
-		# watch_manager : It is the file changes scanner.
-		# Used when a new changed is detected, if it happen,
-		# watch_manager along self.notifier detect changes,
-		# update the queue, and set the new state.
-		watch_manager = pyinotify.WatchManager()
-		watch_manager.add_watch('%s/active' % sys.path[0], pyinotify.IN_MODIFY)
-		self.notifier = pyinotify.ThreadedNotifier(watch_manager, self)
+        # watch_manager : It is the scanner for file changes.
+        # Used when a new change is detected.
+        # watch_manager along self.notifier detect changes,
+        # update the queue, and set the new state.
 
-	def bind(self, address, port):
-		""" Asociate the addres and port with the socket. """
+        self.__init_scope()
 
-		self.address = '%s:%s' % (address, port)
-		try:
-			self.file = open('./active/%s' % self.address, 'x')
-		except Exception as e:
-			print('[Error] Socket in use %s' % self.address)
+        #
+        watch_manager = pyinotify.WatchManager()
+        watch_manager.add_watch(self.sockets_scope, pyinotify.IN_MODIFY)
+        self.notifier = pyinotify.ThreadedNotifier(watch_manager, self)
 
-	def listen(self):
-		""" The socket is now capable of detect changes of its respective file."""
-		self.listening = True
-		self.notifier.start()
+    def __init_scope(self):
+        """Initialize directories for scope of the sockets"""
 
-	def accept(self):
-		pass
+        SOCKET_DIR = "active"
+        self.sockets_scope = Path.joinpath(Path.cwd(), Path(SOCKET_DIR))
 
-	def close(self):
-		""" Close the connection and removes the socket file. """
-		if self.file:
-			self.file.close()
-		os.system('rm ./active/%s' % self.address)
+        if not Path.exists(self.sockets_scope):
+            print("[Warn] Not found. Creating .active directory ...")
+            Path.mkdir(self.sockets_scope)
 
-	def send(self, data):
-		with open('./active/%s' % self.address, 'w') as file:
-			file.write(data)
+    def bind(self, address, port):
+        """ Asociate the addres and port with the socket. """
 
-	def recv(self):
+        self.address = "{}:{}".format(address, port)
 
-		# Guarrantee the queue has new data,
-		# if it is not the case it will be blocked until new data is detected
-		if self.listening:
+        try:
+            self.filepath = Path.joinpath(self.sockets_scope, self.address)
+            self.file = open(self.filepath, 'x')
+        except Exception as e:
+            print("[Error] Socket in use {}".format(self.address))
+            self.file = None
 
-			while self.is_ignored:
-				# Blocking loop
-				pass
+    def listen(self):
+        """ The socket is now capable of detect changes of its respective file."""
+        if self.file:
+            self.listening = True
+            self.notifier.start()
+            print("[Info] Listening on {}".format(self.address))
 
-			data = self.msg_queue[0]
-			self.msg_queue = self.msg_queue[1:]
+    def accept(self):
+        pass
 
-			self.is_ignored = True
-			return data
+    def close(self):
+        """ Close the connection and removes the socket file. """
+        if self.file:
+            self.file.close()
 
-		else:
-			raise Exception('[Err] The socket is not listening')
-			exit()
+        self.filepath.unlink()
 
-	def connect(self, address, port):
-		""" Asociate the addres for use in remote mode. (read only) """
-		address = '%s:%s' % (address, port)
-		try:
-			self.file = open('./active/%s' % address, 'r')
-			self.address = address
-		except Exception as e:
-			print('[Error] Could not connect to %s' % address)
+    def send(self, data):
+        with open(self.filepath, 'w') as file:
+            file.write(data)
 
-	def process_IN_MODIFY(self, event):
-		if self.address == event.pathname.split('/')[-1]:
+    def recv(self):
 
-			with open('./active/%s' % self.address, 'r') as file:
-				data = file.read()
-				self.msg_queue.append(data)
+        # Guarrantee the queue has new data,
+        # if it is not the case it will be blocked until new data is detected
+        if self.listening:
 
-			# Unlock the receiver socket method
-			self.is_ignored = False
+            while self.is_ignored:
+                # Blocking loop
+                pass
+
+            data = self.msg_queue[0]
+            self.msg_queue = self.msg_queue[1:]
+
+            self.is_ignored = True
+            return data
+
+        else:
+            raise Exception('[Error] The socket is not listening')
+            exit()
+
+    def connect(self, address, port):
+        """ Asociate the addres for use in remote mode. (read only) """
+        address = "{}:{}".format(address, port)
+        try:
+        	self.filepath = Path.joinpath(self.sockets_scope, address)
+        	self.file = open(self.filepath, 'r')
+        	self.address = address
+        except Exception as e:
+            print("[Error] Could not connect to {}".format(address))
+
+    def process_IN_MODIFY(self, event):
+        if self.address == event.pathname.split('/')[-1]:
+
+            with open(self.filepath, 'r') as file:
+                data = file.read()
+                self.msg_queue.append(data)
+
+            # Unlock the receiver socket method
+            self.is_ignored = False
--- server.py
+++ server.py
@@ -4,19 +4,18 @@ import isocket
 
 try:
 
-	server = isocket.Socket()
-	server.bind('127.0.0.1', 5000)
-	server.listen()
+    server = isocket.Socket()
+    server.bind('127.0.0.1', 5000)
+    server.listen()
 
-	try:
-		while True:
-			print(server.recv())
+    try:
+        while True:
+            print(server.recv())
 
-	except Exception as e:
-		server.close()
-		print(e)
+    except Exception as e:
+        server.close()
+        print(e)
 
 except KeyboardInterrupt as e:
-	server.close()
-	print(e)
-
+    server.close()
+    print(e)
